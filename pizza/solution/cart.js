5d340ef729ebd7a5c20d781028034f1fbc36a40b
Guillaume Ferrari <guillaume.ferrari@gmail.com>
2020-11-19 00:43:18 +0100

    Ajout bouton Paypal

--- pizza/solution/cart.js
+++ pizza/solution/cart.js
@@ -1,19 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
   let pizzas = document.querySelectorAll("li");
-  pizzas.forEach((pizza) => {
+  let mesPizzas = new Map();
+  pizzas.forEach((pizza, i) => {
+    mesPizzas.set(pizza.dataset.ref, 0);
     pizza.querySelector(".moins").addEventListener("click", () => {
       if (pizza.querySelector(".qt").innerHTML > 0) {
         pizza.querySelector(".qt").innerHTML--;
         pizza.querySelector(".totalligne").innerHTML = pizza.querySelector(".qt").innerHTML * pizza.dataset.prix;
-        document.querySelector("#total").innerHTML = Number(document.querySelector("#total").innerHTML) - Number(pizza.dataset.prix);
-        Number(document.querySelector("#total").innerHTML) > 0 ? (document.querySelector("button").style.display = "block") : (document.querySelector("button").style.display = "none");
+        document.querySelector("#amount").value = Number(document.querySelector("#amount").value) - Number(pizza.dataset.prix);
+        Number(document.querySelector("#amount").value) > 0 ? (document.querySelector("#commande").style.display = "block") : (document.querySelector("#commande").style.display = "none");
+        mesPizzas.set(pizza.dataset.ref, mesPizzas.get(pizza.dataset.ref) - 1);
+        affichercommande(mesPizzas);
       }
     });
     pizza.querySelector(".plus").addEventListener("click", () => {
       pizza.querySelector(".qt").innerHTML++;
       pizza.querySelector(".totalligne").innerHTML = pizza.querySelector(".qt").innerHTML * pizza.dataset.prix;
-      document.querySelector("#total").innerHTML = Number(document.querySelector("#total").innerHTML) + Number(pizza.dataset.prix);
-      Number(document.querySelector("#total").innerHTML) > 0 ? (document.querySelector("button").style.display = "block") : (document.querySelector("button").style.display = "none");
+      document.querySelector("#amount").value = Number(document.querySelector("#amount").value) + Number(pizza.dataset.prix);
+      Number(document.querySelector("#amount").value) > 0 ? (document.querySelector("#commande").style.display = "block") : (document.querySelector("#commande").style.display = "none");
+      mesPizzas.set(pizza.dataset.ref, mesPizzas.get(pizza.dataset.ref) + 1);
+      affichercommande(mesPizzas);
     });
   });
+  function affichercommande(mesPizzas) {
+    let commande = "";
+    for (var [key, value] of mesPizzas) {
+      commande += key + " : " + value + " - ";
+    }
+    document.querySelector("#description").value = commande.slice(0, -3);
+  }
 });
